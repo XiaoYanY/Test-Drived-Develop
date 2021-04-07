d410c3bc0ce39ba20a94ef2ab9cd8ce29a1427f6
刘妍 <yliu@kaikeba.com>
2021-04-07 11:34:00 +0800

    feat:webpack脚手架搭建

--- consts/index.js
+++
@@ -1,7 +0,0 @@
-//方向描述
-exports.Direction = {
-  North: "N",
-  South: "S",
-  West: "W",
-  East: "E",
-}
--- tests/calc.spec.js
+++
@@ -1,4 +0,0 @@
-const sum = require('../src/calc');
-test("calc plus", () => {
-  expect(sum(1, 2)).toBe(3)
-})
